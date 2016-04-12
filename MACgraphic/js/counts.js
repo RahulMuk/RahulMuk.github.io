91d0f0397d3593159bc0561bc24d7e4e5b676a08
RahulMuk <r.mukherjee5@icloud.com>
2016-04-12 05:34:49 -0400

    Initial upload of MAC graphic
    Shows financial information of MAC schools and OSU in Ohio

---
+++ MACgraphic/js/counts.js
@@ -0,0 +1,96 @@
+    var options = {
+  useEasing : true, 
+  useGrouping : true, 
+  separator : ',', 
+  decimal : '.', 
+  prefix : '', 
+  suffix : '%' 
+};
+        var options2 = {
+  useEasing : true, 
+  useGrouping : true, 
+  separator : ',', 
+  decimal : '.', 
+  prefix : '', 
+  suffix : '%' 
+};
+    
+        var options3 = {
+  useEasing : true, 
+  useGrouping : true, 
+  separator : ',', 
+  decimal : '.', 
+  prefix : '$', 
+  suffix : '' 
+};
+
+function osuCount(){
+
+var sub = new CountUp("sub", 0, 0, 0, 1.5, options);
+var atten = new CountUp("atten", 0, 169, 0, 1.5, options2);
+var stud = new CountUp("stud", 0, 0, 0, 1.5, options3);
+sub.start();
+atten.start();
+stud.start();
+}
+
+
+function ouCount(){
+
+var sub = new CountUp("sub", 0, 66.57, 0, 1.5, options);
+var atten = new CountUp("atten", 10, 60, 0, 1.5, options2);
+var stud = new CountUp("stud", 0, 539.02, 0, 1.5, options3);
+sub.start();
+atten.start();
+stud.start();
+}
+
+function ksuCount(){
+
+var sub = new CountUp("sub", 0, 76.36, 0, 1.5, options);
+var atten = new CountUp("atten", 0, 39, 0, 1.5, options2);
+var stud = new CountUp("stud", 0, 549.6, 0, 1.5, options3);
+sub.start();
+atten.start();
+stud.start();
+}
+
+function mamCount(){
+
+var sub = new CountUp("sub", 0, 71.84, 0, 1.5, options);
+var atten = new CountUp("atten", 0, 78, 0, 1.5, options2);
+var stud = new CountUp("stud", 0, 1036.17, 0, 1.5, options3);
+sub.start();
+atten.start();
+stud.start();
+}
+
+function bgsuCount(){
+
+var sub = new CountUp("sub", 0, 62.73, 0, 1.5, options);
+var atten = new CountUp("atten", 0, 79, 0, 1.5, options2);
+var stud = new CountUp("stud", 0, 762.19, 0, 1.5, options3);
+sub.start();
+atten.start();
+stud.start();
+}
+
+function utCount(){
+
+var sub = new CountUp("sub", 0, 57.36, 0, 1.5, options);
+var atten = new CountUp("atten", 0, 84, 0, 1.5, options2);
+var stud = new CountUp("stud", 0, 554.37, 0, 1.5, options3);
+sub.start();
+atten.start();
+stud.start();
+}
+
+function uaCount(){
+
+var sub = new CountUp("sub", 0, 69.12, 0, 1.5, options);
+var atten = new CountUp("atten", 0, 31, 0, 1.5, options2);
+var stud = new CountUp("stud", 0, 773.54, 0, 1.5, options3);
+sub.start();
+atten.start();
+stud.start();
+}
