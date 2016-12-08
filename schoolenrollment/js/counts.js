7dce3faeee5ce2bbfde349dde03385b1ac6e74d0
RahulMuk <r.mukherjee5@icloud.com>
2016-12-08 15:35:58 -0500

    Uploaded School Enrollment Project

---
+++ schoolenrollment/js/counts.js
@@ -0,0 +1,96 @@
+    var options = {
+  useEasing : true, 
+  useGrouping : true, 
+  separator : ',', 
+  decimal : '.', 
+  prefix : '', 
+  suffix : '%' 
+};
+        var options2 = {
+  useEasing : true, 
+  useGrouping : true, 
+  separator : ',', 
+  decimal : '.', 
+  prefix : '', 
+  suffix : '%' 
+};
+    
+        var options3 = {
+  useEasing : true, 
+  useGrouping : true, 
+  separator : ',', 
+  decimal : '.', 
+  prefix : '$', 
+  suffix : ' Million' 
+};
+
+function osuCount(){
+
+var sub = new CountUp("sub", 0, 15, 0, 1.5, options);
+var atten = new CountUp("atten", 0, 53, 0, 1.5, options2);
+var stud = new CountUp("stud", 0, 17, 0, 1.5, options3);
+sub.start();
+atten.start();
+stud.start();
+}
+
+
+function ouCount(){
+
+var sub = new CountUp("sub", 0, 27, 0, 1.5, options);
+var atten = new CountUp("atten", 10, 80, 0, 1.5, options2);
+var stud = new CountUp("stud", 0, 2.7, 0, 1.5, options3);
+sub.start();
+atten.start();
+stud.start();
+}
+
+function ksuCount(){
+
+var sub = new CountUp("sub", 0, 12, 0, 1.5, options);
+var atten = new CountUp("atten", 0, 78, 0, 1.5, options2);
+var stud = new CountUp("stud", 0, 2, 0, 1.5, options3);
+sub.start();
+atten.start();
+stud.start();
+}
+
+function mamCount(){
+
+var sub = new CountUp("sub", 0, 40, 0, 1.5, options);
+var atten = new CountUp("atten", 0, 56, 0, 1.5, options2);
+var stud = new CountUp("stud", 0, 3, 0, 1.5, options3);
+sub.start();
+atten.start();
+stud.start();
+}
+
+function bgsuCount(){
+
+var sub = new CountUp("sub", 0, 23, 0, 1.5, options);
+var atten = new CountUp("atten", 0, 87, 0, 1.5, options2);
+var stud = new CountUp("stud", 0, 3.4, 0, 1.5, options3);
+sub.start();
+atten.start();
+stud.start();
+}
+
+function utCount(){
+
+var sub = new CountUp("sub", 0, 34, 0, 1.5, options);
+var atten = new CountUp("atten", 0, 67, 0, 1.5, options2);
+var stud = new CountUp("stud", 0, 5.7, 0, 1.5, options3);
+sub.start();
+atten.start();
+stud.start();
+}
+
+function uaCount(){
+
+var sub = new CountUp("sub", 0, 18, 0, 1.5, options);
+var atten = new CountUp("atten", 0, 92, 0, 1.5, options2);
+var stud = new CountUp("stud", 0, 7.8, 0, 1.5, options3);
+sub.start();
+atten.start();
+stud.start();
+}
